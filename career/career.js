545bb65e86e2b1b08c67ad33c026be325edb16bf
Khush Mahawar <khushmahawar221@gmail.com>
2026-03-20 03:41:16 +0530

    initial commit

---
+++ career/career.js
@@ -0,0 +1,43 @@
+function goToLogin() {
+  window.location.href = "/login";
+}
+
+document.getElementById("jobForm").addEventListener("submit", function(e){
+  e.preventDefault();
+  const loader = document.getElementById("loader");
+  loader.classList.add("show");
+  const nameVal = document.getElementById("name").value;
+  const emailVal = document.getElementById("email").value;
+  const collegeVal = document.getElementById("college").value;
+  const cpiVal = document.getElementById("cpi").value;
+  const phoneVal = document.getElementById("phone").value;
+
+  fetch("https://script.google.com/macros/s/AKfycbygedLkIw1WGOqSiNtp7enAXAzvOV51pdJUZkweKElDhhfEu9A49sOFlRxGUjdCyF7OCg/exec", {
+    method: "POST",
+    body: JSON.stringify({
+      name: nameVal,
+      email: emailVal,
+      cpi: cpiVal,
+      college: collegeVal,
+      phone: phoneVal
+    })
+  })
+  .then(res => res.text())
+.then((response) => {
+  loader.classList.remove("show");
+
+  const errorBox = document.getElementById("errorBox");
+
+  if (response.trim() === "EXISTS") {
+    // ❌ Show error box
+    errorBox.classList.add("show");
+
+    // 🧹 Form reset
+    document.getElementById("jobForm").reset();
+
+  } else {
+    window.location.href = "/applied";
+  }
+})
+});
+
---
+++ script.js
@@ -0,0 +1,56 @@
+
+  /*// Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.10.0/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.10.0/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyCRt_nj4Svk4yfZBeQ8jcSr_VQXWrhTqkY",
+    authDomain: "smartconsult-employee.firebaseapp.com",
+    projectId: "smartconsult-employee",
+    storageBucket: "smartconsult-employee.firebasestorage.app",
+    messagingSenderId: "595014566518",
+    appId: "1:595014566518:web:e3ed37368e5026fd75d557",
+    measurementId: "G-B82NX4VPZR"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+*/
+function goToApply() {
+  window.location.href = "/career";
+}
+
+function goToLogin() {
+  window.location.href = "/login";
+}
+// Set deadline (change date as needed)
+const deadline = new Date().getTime() + (14 * 24 * 60 * 60 * 1000); // 14 days from now
+
+function updateTimer() {
+  const now = new Date().getTime();
+  const distance = deadline - now;
+
+  if (distance < 0) {
+    document.getElementById("timer").innerHTML = "Applications Closed ❌";
+    return;
+  }
+
+  const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((distance / (1000 * 60 * 60)) % 24);
+  const minutes = Math.floor((distance / (1000 * 60)) % 60);
+  const seconds = Math.floor((distance / 1000) % 60);
+
+  document.getElementById("days").innerText = days;
+  document.getElementById("hours").innerText = hours;
+  document.getElementById("minutes").innerText = minutes;
+  document.getElementById("seconds").innerText = seconds;
+}
+
+updateTimer();
+setInterval(updateTimer, 1000);
+
